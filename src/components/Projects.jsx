bbb26c99fceb37ec317e2c6a2348282a1dbdf682
jeppe314 <jesperlindqvist93@gmail.com>
2022-12-10 14:13:27 +0100

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Project } from "./Project"
 import { ProjectsData as data } from "./ProjectsData"
 import { nanoid } from "nanoid"
-import waveBtm from "../assets/waveBtm.svg"
 
 export const Projects = () => {
     const projectsEl = data.map((project) => {
@@ -19,7 +18,7 @@ export const Projects = () => {
 
     return (
         <section className="section--projects">
-            <img src={waveBtm} alt="morjens" className="top" />
+
             <div className="projects--title">
                 <h1>Projects</h1>
             </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import waveTop from "../assets/waveTop.svg"
+import waveBtm from "../assets/waveBtm.svg"
 
 export const Skills = () => {
     return (
         <section className="section--skills">
             <img src={waveTop} alt="morjens" className="top" />
-            <h1>Core technologies</h1>
+            {/* <h1>Core technologies</h1> */}
             <div className="skillgrid">
                 <i className="devicon-html5-plain colored"></i>
                 <i className="devicon-css3-plain colored"></i>
@@ -13,6 +14,7 @@ export const Skills = () => {
                 <i className="devicon-react-original colored"></i>
                 <i className="devicon-git-plain colored"></i>
             </div>
+            <img src={waveBtm} alt="morjens" className="btm" />
         </section>
     )
 }
