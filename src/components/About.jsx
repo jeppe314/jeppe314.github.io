6b0d971cd5a7a4cf2f408af72277f9787d7291dd
jeppe314 <jesperlindqvist93@gmail.com>
2022-12-11 11:05:48 +0100

    css updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,13 @@ import JesperObject from "./JesperObject"
 export const About = () => {
     return (
         <section className="section--about" style={{ position: "relative" }}>
-            <div className="about--text"></div>
+            <div className="about--text">
+                <img
+                    src="./images/jesperiskogen.jpg"
+                    alt="jesper"
+                    className="jesper-img"
+                />
+            </div>
             <div className="jesper--div">
                 <JesperObject />
             </div>
